cf7096aff20e783d52eeb4f3d0bbd1162ed34af6
danilojha <danilojha@gmail.com>
2019-02-03 13:34:12 -0500

    some work on the descent algo

--- starter.py
+++ starter.py
@@ -23,16 +23,10 @@ def loadData():
 
 def MSE(W, b, x, y, reg):   
     N = len(x)
-    x = np.reshape(x, (N, np.shape(x)[1]*np.shape(x)[2]))
-    print(np.shape(x))
-    print(np.shape(y))
-    print(np.shape(np.transpose(W)))
     #MSE loss function
     Ld = (1/(2*N))*(np.square(np.linalg.norm(np.matmul(x,W) + b - y)))
-    print(Ld)
     #Weight decay loss function
     Lw = (reg/2)*np.square(np.linalg.norm(W))
-    print(Lw)
     #Total Loss
     Loss = Lw + Ld
     return Loss
@@ -40,17 +34,17 @@ def MSE(W, b, x, y, reg):
 def gradMSE(W, b, x, y, reg):
 #Gradient with respect to weights
     N = len(x)
-    x = np.reshape(x, (N, np.shape(x)[1]*np.shape(x)[2]))
-    a=np.matmul(x, W)
-    gradW = np.linalg.norm(a + b - y)*x + reg*np.linalg.norm(W)
+    Wx = np.matmul(x, W)
+    L2 = np.linalg.norm(Wx + b - y)
+    #gradient of MSE with respect to weights
+    gradW = np.zeros((N,1))
     print(np.shape(gradW))
-    
-    gradB = np.linalg.norm(a + b - y)
-    print(np.shape(gradB))
-    print(gradB)
+    gradW = (1/N)*L2*x.T + reg*np.linalg.norm(W)
+    print(np.shape(gradW))
+    print(np.shape(x))
+    #gradient of MSE with respect to bias
+    gradB = (1/N)*L2
     return gradW, gradB
-
-
 
 #def crossEntropyLoss(W, b, x, y, reg):
     # Your implementation here
@@ -58,8 +52,18 @@ def gradMSE(W, b, x, y, reg):
 #def gradCE(W, b, x, y, reg):
     # Your implementation here
 
-#def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
-    # Your implementation here
+def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
+    x = trainingData
+    y = trainingLabels
+    print(np.shape(W))
+    for i in range(iterations):
+        gradW, gradB = gradMSE(W, b, x, y, reg)
+        if np.all(gradW < EPS):
+            return W, b
+        W = W - alpha*gradW.T
+        b = b - alpha*gradB
+        print(i)
+    return W, b
 
 #def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
     # Your implementation here
@@ -67,10 +71,19 @@ def gradMSE(W, b, x, y, reg):
 trainData, validData, testData, trainTarget, validTarget, testTarget = loadData();
 W = np.zeros((784, 1))
 x = trainData
+N = len(x)
+x = np.reshape(x, (N, np.shape(x)[1]*np.shape(x)[2]))
 y = trainTarget
 #print(W)
 
-MSE = MSE(W, 0, x, y, 0)
-print(MSE)
+
+
+mean = MSE(W, 0, x, y, 0)
+print(mean)
 grad = gradMSE(W, 0, x, y, 0)
+#W, b = grad_descent(W, 0, x, y, 0.005, 5000, 0, 0.0000001)
+mean = MSE(W, 0, x, y, 0)
+eff = 1 - mean
+print(eff)
+
 
