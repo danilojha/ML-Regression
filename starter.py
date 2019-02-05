f850e02d77c53cbf3d0f4b6720569439a5444d26
Beth <elizabeth.bertozzi@mail.utoronto.ca>
2019-02-05 17:13:44 -0500

    Wrote build graph and started sdg

--- starter.py
+++ starter.py
@@ -78,14 +78,14 @@ def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS
     return W, b
 
 def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
-`
+
     #Initialize tensors
-    W = tf.truncated_normal(shape=[784], stddev=0.5, dtype=tf.float32)
-    b = tf.Variable()
-    x = tf.placeholder(tf.float32, shape=())
-    y = tf.placeholder(tf.float32, shape=())
-    reg = tf.placeholder(tf.float32, shape=())
-    learning_rate =
+    W = tf.truncated_normal(shape=[784], stddev=0.5, dtype=tf.float32) #weights
+    b = tf.Variable() #bias
+    x = tf.placeholder(tf.float32, shape=(3500,784)) #data
+    yhat = tf.placeholder(tf.float32, shape=(3500, 1)) #predicted labels
+    y = tf.placeholder(tf.float32, shape=(3500, 1)) #real labels
+    reg = tf.placeholder(tf.float32, shape=(1)) #regularization param
     
     tf.set_random_seed(421)
 
@@ -94,10 +94,32 @@ def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rat
         L = MSE(W, b, x, y, reg)
     elif loss == "CE":
         L = crossEntropyLoss(W, b, x, y, reg)
-    opt = GradientDescentOptimizer(learning_rate=0.001)
-    opt_op = opt.minimize(L)
 
-    # Your implementation here
+    opt = tf.train.AdamOptimizer(learning_rate=0.001,beta1, beta2, epsilon)
+    opt_op = opt.minimize(L)
+    #Be sure to run opt_op.run() in training
+    return W, b, yhat, y, L, opt, reg
+
+def SDG(W, b, x, yhat, y, L, opt, reg, batchSize, epoch)
+    N = len(x)
+    num_mini_batches = math.floor(N/batchSize)
+    _permutation = np.random.permutation(N)
+    for i in range(epochs)
+        shuffled_x = x[:,_permutation]
+        shuffled_y =
+        
+        for j in range(num_mini_batches)
+            #get mini-batches
+            mini_batch_x = shuffled_x[:,j*batchSize : (j+1)*batchSize]
+            mini_batch_y = shuffled_y[:,j*batchSize : (j+1)*batchSize]
+            #Calculate with update with mini-batch
+            Loss = 
+
+
+
+
+
+        #Store the training, validation and test losses and accuracies
 
 
 
@@ -131,12 +153,7 @@ print(1 - validOld)
 print(1- LossValid)
 
 
-TestgradCE = gradCE(W, 0, x, y, 0)
-TestgradMSE = gradMSE(W, 0, x, y, 0)
-print(np.shape(TestgradCE))
-print(TestgradCE)
-print(np.shape(TestgradMSE))
-print(TestgradMSE)
+
 
 
 
