77aa25e4903d7e409920cd6340edc2c372dd3878
danilojha <danilojha@gmail.com>
2019-02-06 14:30:32 -0500

    changes to buildGraph

--- starter.py
+++ starter.py
@@ -94,31 +94,31 @@ def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS
             return W, b
     return W, b
 
-def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
-    #Initialize tensors
-    W = tf.truncated_normal(shape=[784], stddev=0.5, dtype=tf.float32) #weights
-    b = tf.Variable(1) #bias
-    x = tf.placeholder(tf.float32, shape=(3500,784)) #data
-    yhat = tf.placeholder(tf.float32, shape=(3500, 1)) #predicted labels
-    y = tf.placeholder(tf.float32, shape=(3500, 1)) #real labels
-    reg = tf.placeholder(tf.float32, shape=(1)) #regularization param
-    
+def buildGraph(beta1=None, beta2=None, epsilon=None, lossType="None", learning_rate=None):
     tf.set_random_seed(421)
+    #Initialize tensors
+    W = tf.Variable(truncated_normal(shape=[784, 1], stddev=0.5), name='weight') #weights
+    b = tf.Variable(0, name = 'bias') #bias
+    x = tf.placeholder(tf.float32, shape=(3500,784), name='x') #data
+    y = tf.placeholder(tf.float32, shape=(3500, 1), name='y') #real labels 
+    reg = tf.placeholder(tf.float32, shape=(1), name ='reg') #regularization
+    
+    yhat = tf.placeholder(tf.float32, shape=(3500, 1), name = 'yhat') #predicted labels
+    yhat = tf.math.add(tf.math.multiply(x, W))
+    
     if loss == "MSE":
-        L = MSE(W, b, x, y, reg)
+        Loss = tf.losses.mean_squared_error(y,yhat)
     elif loss == "CE":
-        L = crossEntropyLoss(W, b, x, y, reg)
+        Loss = tf.losses.sigmoid_cross_entropy(y, yhat)
 
-    opt = tf.train.AdamOptimizer(learning_rate=0.001).minimize(L)
-
-    #Initialize variables? I think
-    sess.run(tf.global_variables_initializer())
+    opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
     #Be sure to run opt_op.run() in training
-    return W, b, yhat, y, L, opt, reg
+    return W, b, x, yhat, y, Loss, opt, reg
 
-def SGD(W, b, x, yhat, y, L, opt, reg, batchSize, epoch):
+def SGD(W, b, x, y, alpha, iterations, reg, EPS, lossType="None", batchSize, epoch):
     #Initialize graph
-    w, bias, _y, y, loss, optimizer, regExp = buildGraph(beta1=None, beta2=None, epsilon=None, lossType="MSE", learning_rate=0.001)
+    W, b, x, yhat, y, Loss, opt, reg = buildGraph(beta1=None, beta2=None, epsilon=None, lossType="MSE", learning_rate=0.001)
+    sess.run(tf.global_variables_initializer())
     #Calculate number of batches in training set
     N = len(x)
     num_mini_batches = math.floor(N/batchSize)
@@ -149,8 +149,7 @@ trainData, validData, testData, trainTarget, validTarget, testTarget = loadData(
 
 #initializing parameters
 W = np.zeros((784, 1))
-x = trainData
-N = len(x)
+N = len(trainData)
 b = 0
 epochs = 5000
 error = 0.0000001
@@ -158,11 +157,14 @@ LR = 0.005
 reg = 0.1
 
 #reshaping data
-x = np.reshape(x, (N, np.shape(x)[1]*np.shape(x)[2]))
+trainData = np.reshape(trainData, (len(trainData), np.shape(trainData)[1]*np.shape(trainData)[2]))
 testData = np.reshape(testData, (len(testData), np.shape(testData)[1]*np.shape(testData)[2]))
 validData = np.reshape(validData, (len(validData), np.shape(validData)[1]*np.shape(validData)[2]))
 
+x = trainData
 y = trainTarget
+
+
 
 
 
